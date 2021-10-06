b71925159a5985fac91778d4d65d9eaf6bbfd163
Andrew Albers <asalbers@gmail.com>
2021-10-05 20:42:08 -0500

    testing

--- vars/deployTerraform.groovy
+++ vars/deployTerraform.groovy
@@ -19,7 +19,20 @@ def tfapplyk8s(){
                         export TF_VAR_client_id=$AZURE_CLIENT_ID
                         export TF_VAR_client_secret=$AZURE_CLIENT_SECRET
                         terraform apply -input=false tfplan
-                        terraform destroy -input=false
+                    '''
+                }
+}
+
+def tfdestroyk8s(){
+         withCredentials([azureServicePrincipal('azure_id')]) {
+                    sh  '''
+                        export ARM_CLIENT_ID=$AZURE_CLIENT_ID
+                        export ARM_CLIENT_SECRET=$AZURE_CLIENT_SECRET
+                        export ARM_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID
+                        export ARM_TENANT_ID=$AZURE_TENANT_ID
+                        export TF_VAR_client_id=$AZURE_CLIENT_ID
+                        export TF_VAR_client_secret=$AZURE_CLIENT_SECRET
+                        terraform destroy -input=false 
                     '''
                 }
 }
