f9fb31af20cc5f93a3e4f3ec50d09467a67e5358
Andrew Albers <asalbers@gmail.com>
2021-10-05 19:42:22 -0500

    testing

--- vars/terraformTest.groovy
+++ vars/terraformTest.groovy
@@ -27,6 +27,8 @@ def tfplank8s(){
                         export ARM_CLIENT_SECRET=$AZURE_CLIENT_SECRET
                         export ARM_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID
                         export ARM_TENANT_ID=$AZURE_TENANT_ID
+                        export TF_VAR_client_id=$AZURE_CLIENT_ID
+                        export TF_VAR_client_secret=$AZURE_CLIENT_SECRET
                         terraform plan
                     '''
                 }
